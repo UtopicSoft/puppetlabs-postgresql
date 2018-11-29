cde258c6180a571000b94506b9842fd258359a60
UtopicSoft <tc@n-ix.com>
2018-11-29 11:56:27 -0500

    Update describe.pp

--- manifests/describe.pp
+++ manifests/describe.pp
@@ -1,10 +1,10 @@
 #
-# @name: postgresql-server 
-# @type: package
-# @desc: Install postgresql server package and manages it on the host. Package will be _reinstalled_ if removed.
-#
-#
-# @name: /etc/sysconfig/pgsql/postgresql
-# @type: file
-# @desc: RedHat-based systems hardcode some PG* variables in the init script.
+#+ @name: postgresql-server 
+#+ @type: package
+#+ @desc: Install postgresql server package and manages it on the host. Package will be _reinstalled_ if removed.
+#+
+#+
+#+ @name: /etc/sysconfig/pgsql/postgresql
+#+ @type: file
+#+ @desc: RedHat-based systems hardcode some PG* variables in the init script.
 #
