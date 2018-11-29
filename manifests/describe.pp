d36166be7be399f99e7c93a9a1430fcd38c4cb78
UtopicSoft <tc@n-ix.com>
2018-11-29 13:55:11 -0500

    Update describe.pp

--- manifests/describe.pp
+++ manifests/describe.pp
@@ -7,4 +7,15 @@
 #+ @name: /etc/sysconfig/pgsql/postgresql
 #+ @type: file
 #+ @description: RedHat-based systems hardcode some PG* variables in the init script.
-#
+#+
+#+ @name: /var/lib/pgsql/data/pg_hba.conf
+#+ @type: file
+#+ @description: Client authentication configuration file,
+#+
+#+ @name: /var/lib/pgsql/data/pg_ident.conf
+#+ @type: file
+#+ @description: User name maps configuration file.
+#+
+#+ @name: /var/lib/pgsql/data/postgresql.conf
+#+ @type: file
+#+ @description:  Postgre configuration file.
